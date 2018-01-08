d19d7d32978a1735e98a4bdf793b8cca6dbb8de2
Teppei Kanayana <1994teppei0615kanayama@gmail.com>
2018-01-08 16:30:26 +0900

    ldaの問題完成

--- lda/lda.py
+++ lda/lda.py
@@ -1,30 +1,28 @@
 import numpy as np
 import numpy.linalg as LA
 import matplotlib.pyplot as plt
-import pdb
 
 np.random.seed(0)
-data1 = np.random.rand(50, 2) + np.array([1.2, 0.0])
+
+data1 = np.random.rand(50, 2) + np.array([0.7, 0.0])
 data2 = np.random.rand(50, 2)
 
-def calc_sb(data1, data2):
-    mu = np.mean(data1, axis=0) - np.mean(data2, axis=0)
-    return mu[np.newaxis].T @ mu[np.newaxis]
-
-def calc_sk(data):
-    return np.cov(data.T) * len(data)
-
-def calc_sw(data1, data2):
-    return calc_sk(data1) + calc_sk(data2)
-
 def calc_w(data1, data2):
-    sb = calc_sb(data1, data2)
-    sw = calc_sw(data1, data2)
-    s = LA.inv(sw) @ sb
-    rambda, v = LA.eig(s)
-    return v[np.argmax(rambda)]
+    """2クラスの線型判別分析における、写像ベクトルwを計算する関数。
 
-def draw_figure(data_list, filename, w=None, flag=False):
+    wは、はじパタP.81 (6.26)式における最大固有値に対応する固有ベクトルとして計算する。
+
+    Args:
+        data1 (numpy.ndarray): 1クラス目に属するデータのデータ行列。shape=(N1, d)
+        data2 (numpy.ndarray): 2クラス目に属するデータのデータ行列。shape=(N2, d)
+
+    Return:
+        d次元空間から1次元空間への写像を行う写像ベクトルw
+    """
+    ### TASK ###
+    pass
+
+def draw_figure(data_list, filename, flag=False):
     color = ["blue", "red", "blue", "red"]
     shape = ["*", "+", "*", "+"]
 
@@ -36,11 +34,6 @@ def draw_figure(data_list, filename, w=None, flag=False):
             y = data[:, 1]
         plt.scatter(x, y, c=color[i], marker=shape[i])
 
-    if w is not None:
-        x = np.linspace(0,2,4)
-        y = - (w[0] / w[1]) * x
-        plt.plot(x,y,"r-")
-
     plt.savefig(filename)
     plt.show()
 
@@ -48,7 +41,7 @@ def main():
     w = calc_w(data1, data2)
     data1_1d = data1 @ w[np.newaxis].T
     data2_1d = data2 @ w[np.newaxis].T
-    draw_figure([data1, data2, data1_1d, data2_1d], "after.png", w=w, flag=True)
+    draw_figure([data1, data2, data1_1d, data2_1d], "lda.png", flag=True)
 
 if __name__ == "__main__":
     main()
